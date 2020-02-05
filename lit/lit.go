951b37d62c4481bf50f32559ee67a4fb26b1d7df
Matt Hale <matthew.hale@protonmail.com>
2020-02-05 14:27:34 -0500

    Add basic stdin input func

--- lit/lit.go
+++ lit/lit.go
@@ -1,7 +1,23 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"bufio"
+	"os"
+)
+
+func input() []string {
+	in := make([]string, 0)
+	scanner := bufio.NewScanner(os.Stdin)
+	for scanner.Scan() {
+		in = append(in, scanner.Text())
+	}
+	return in
+}
 
 func main() {
-	fmt.Println("Hello, world.")
+	input := input()
+	for _, line := range input {
+		fmt.Println(line)
+	}
 }
