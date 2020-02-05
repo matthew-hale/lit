730a34a10e4c60db8bb7d545368b711564b775c4
Matt Hale <matthew.hale@protonmail.com>
2020-02-05 15:34:17 -0500

    Add file start matching functionality to main()

--- lit/lit.go
+++ lit/lit.go
@@ -20,9 +20,21 @@ func input() []string {
 func main() {
 	input := input()
 	fileStartMatch := regexp.MustCompilePOSIX("^```[a-zA-Z0-9 _\\-]+\\.?[a-zA-Z]*$")
-	for _, line := range input {
+	fileEndMatch := regexp.MustCompilePOSIX("^```$")
+	fileIndexes:= make([]int, 0)
+	for i, line := range input {
 		if fileStartMatch.MatchString(line) {
-			fmt.Println(strings.Trim(line, "```"))
+			fileIndexes = append(fileIndexes, i)
+		}
+	}
+	for _, index := range fileIndexes {
+		filename := strings.Trim(input[index], "```")
+		fmt.Println(filename)
+		for _, line := range input[index+1:] {
+			if fileEndMatch.MatchString(line) {
+				break
+			}
+			fmt.Println(line)
 		}
 	}
 }
